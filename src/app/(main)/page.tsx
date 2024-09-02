b6b3afc35faf620f3237e65b64c524873b4b7354
Tuyulll <19623185@std.stei.itb.ac.id>
2024-09-03 05:18:07 +0700

    setup db + prisma + OAuth Google

---
+++ src/app/(main)/page.tsx
@@ -0,0 +1,14 @@
+import SignIn from "@/app/(main)/sign-in"
+
+export default function Home() {
+    return (
+      <div>
+        <main>
+            <SignIn/>
+        </main>
+        <footer>
+        </footer>
+      </div>
+    );
+  }
+  

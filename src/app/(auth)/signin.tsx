c2f52b515842091e1bc56b316b4fe067e91e6479
Tuyulll <19623185@std.stei.itb.ac.id>
2024-09-05 17:38:23 +0700

    feat: error messege

--- src/app/(auth)/signin.tsx
+++ src/app/(auth)/signin.tsx
@@ -2,11 +2,24 @@
 
 import { Button } from "@/components/ui/button";
 import { signIn } from "next-auth/react";
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 import { FaGoogle } from "react-icons/fa";
 
 export default function SignIn() {
+  const searchParams = useSearchParams();
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (searchParams) {
+      const errorParam = searchParams.get("error");
+      if (errorParam) {
+        setError(errorParam);
+      }
+    }
+  }, [searchParams]);
   return (
-    <div className="flex h-2/3 flex-auto items-center justify-center border-2 md:h-full">
+    <div className="flex h-2/3 flex-auto flex-col items-center justify-center border-2 md:h-full">
       <Button
         variant={"ghost"}
         onClick={() => signIn("google")}
@@ -15,6 +28,9 @@ export default function SignIn() {
         <FaGoogle size={16} className="mr-2" />
         <p>Sign In</p>
       </Button>
+      {error && (
+        <p className="text-center font-bold text-destructive">{error}</p>
+      )}
     </div>
   );
 }
--- src/auth.ts
+++ src/auth.ts
@@ -1,9 +1,80 @@
-import NextAuth from "next-auth";
+import NextAuth, { DefaultSession } from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/lib/prisma";
 import Google from "next-auth/providers/google";
 
+declare module "next-auth" {
+  interface Session extends DefaultSession {
+    user: {
+      userName: string;
+    } & DefaultSession["user"];
+  }
+}
+
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
+  pages: {
+    signIn: "/",
+    error: "/",
+  },
   providers: [Google],
+  callbacks: {
+    async signIn({ user }) {
+      const existingUser = await prisma.user.findUnique({
+        where: { id: user.id },
+      });
+
+      if (existingUser && !existingUser.userName) {
+        const generateUserName = (name: string): string[] => {
+          const sanitized = name.toLowerCase().replace(/\s+/g, "");
+          const possibleUserNames = [
+            sanitized,
+            sanitized.replace(" ", "_"),
+            `${sanitized}${Math.floor(Math.random() * 1000000)}`,
+          ];
+          return possibleUserNames;
+        };
+
+        let newUserName = generateUserName(user.name || "")[0];
+        let isUnique = false;
+        let attempt = 0;
+
+        while (!isUnique) {
+          const checkUser = await prisma.user.findUnique({
+            where: { userName: newUserName },
+          });
+
+          if (!checkUser) {
+            isUnique = true;
+          } else {
+            attempt++;
+            const possibleUserNames = generateUserName(user.name || "");
+            newUserName = possibleUserNames[attempt % possibleUserNames.length];
+          }
+        }
+
+        await prisma.user.update({
+          where: { id: user.id },
+          data: { userName: newUserName },
+        });
+      }
+
+      return true;
+    },
+    async session({ session, user }) {
+      const userData = await prisma.user.findUnique({
+        where: { id: user.id },
+        select: { userName: true },
+      });
+
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          id: user.id,
+          userName: userData?.userName,
+        },
+      };
+    },
+  },
 });

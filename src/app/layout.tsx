9463ed0b016219c2ce9cd313eb558f88213c6fcf
Tuyulll <19623185@std.stei.itb.ac.id>
2024-09-06 14:21:28 +0700

    fix: default them -> dark

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
         <SessionProvider>
           <ThemeProvider
             attribute="class"
-            defaultTheme="system"
+            defaultTheme="dark"
             enableSystem
             disableTransitionOnChange
           >

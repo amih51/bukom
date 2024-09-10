074e6961db0c8bf89a5d53848ed0f61866077335
Tuyulll <19623185@std.stei.itb.ac.id>
2024-09-10 11:25:12 +0700

    feat: signout alert

--- src/components/nav/profile-btn.tsx
+++ src/components/nav/profile-btn.tsx
@@ -26,6 +26,17 @@ import {
   PiSunFill,
 } from "react-icons/pi";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "../ui/alert-dialog";
 
 export default function ProfileButton() {
   const { data: session } = useSession();
@@ -36,75 +47,94 @@ export default function ProfileButton() {
     const user = session.user;
 
     return (
-      <DropdownMenu open={isDropdownOpen} onOpenChange={setIsDropdownOpen}>
-        <DropdownMenuTrigger asChild>
-          <Button
-            onClick={() => setIsDropdownOpen(!isDropdownOpen)}
-            variant={"ghost"}
-            className="size-full justify-start overflow-hidden p-2 lg:px-4"
-            size={"icon"}
-          >
-            <Avatar>
-              <AvatarImage src={user?.image || ""} />
-              <AvatarFallback>{user?.username}</AvatarFallback>
-            </Avatar>
-            <div className="ml-4 hidden flex-col overflow-hidden text-left lg:flex">
-              <p className="truncate text-lg">{user?.name}</p>
-              <p className="truncate text-sm opacity-50">@{user?.username}</p>
-            </div>
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent>
-          <DropdownMenuSub>
-            <DropdownMenuSubTrigger className="cursor-pointer">
-              <PiCircleHalfFill className="mr-2 size-4" />
-              Theme
-            </DropdownMenuSubTrigger>
-            <DropdownMenuPortal>
-              <DropdownMenuSubContent>
-                <DropdownMenuItem
-                  onClick={() => setTheme("system")}
-                  className="cursor-pointer"
-                >
-                  {theme === "system" ? (
-                    <PiMonitorFill className="mr-2 size-4" />
-                  ) : (
-                    <PiMonitor className="mr-2 size-4" />
-                  )}
-                  System default
-                </DropdownMenuItem>
-                <DropdownMenuItem
-                  onClick={() => setTheme("light")}
-                  className="cursor-pointer"
-                >
-                  {theme === "light" ? (
-                    <PiSunFill className="mr-2 size-4" />
-                  ) : (
-                    <PiSun className="mr-2 size-4" />
-                  )}
-                  Light
-                </DropdownMenuItem>
-                <DropdownMenuItem
-                  onClick={() => setTheme("dark")}
-                  className="cursor-pointer"
-                >
-                  {theme === "dark" ? (
-                    <PiMoonFill className="mr-2 size-4" />
-                  ) : (
-                    <PiMoon className="mr-2 size-4" />
-                  )}
-                  Dark
-                </DropdownMenuItem>
-              </DropdownMenuSubContent>
-            </DropdownMenuPortal>
-          </DropdownMenuSub>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem className="group cursor-pointer">
-            <PiSignOut className="mr-2 size-4" />
-            <button onClick={() => signOut()}>Sign Out</button>
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
+      <AlertDialog>
+        <DropdownMenu open={isDropdownOpen} onOpenChange={setIsDropdownOpen}>
+          <DropdownMenuTrigger asChild>
+            <Button
+              onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+              variant={"ghost"}
+              className="size-full justify-start overflow-hidden p-2 lg:px-4"
+              size={"icon"}
+            >
+              <Avatar>
+                <AvatarImage src={user?.image || ""} />
+                <AvatarFallback>{user?.username}</AvatarFallback>
+              </Avatar>
+              <div className="ml-4 hidden flex-col overflow-hidden text-left lg:flex">
+                <p className="truncate text-lg">{user?.name}</p>
+                <p className="truncate text-sm opacity-50">@{user?.username}</p>
+              </div>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuSub>
+              <DropdownMenuSubTrigger className="cursor-pointer">
+                <PiCircleHalfFill className="mr-2 size-4" />
+                Theme
+              </DropdownMenuSubTrigger>
+              <DropdownMenuPortal>
+                <DropdownMenuSubContent>
+                  <DropdownMenuItem
+                    onClick={() => setTheme("system")}
+                    className="cursor-pointer"
+                  >
+                    {theme === "system" ? (
+                      <PiMonitorFill className="mr-2 size-4" />
+                    ) : (
+                      <PiMonitor className="mr-2 size-4" />
+                    )}
+                    System default
+                  </DropdownMenuItem>
+                  <DropdownMenuItem
+                    onClick={() => setTheme("light")}
+                    className="cursor-pointer"
+                  >
+                    {theme === "light" ? (
+                      <PiSunFill className="mr-2 size-4" />
+                    ) : (
+                      <PiSun className="mr-2 size-4" />
+                    )}
+                    Light
+                  </DropdownMenuItem>
+                  <DropdownMenuItem
+                    onClick={() => setTheme("dark")}
+                    className="cursor-pointer"
+                  >
+                    {theme === "dark" ? (
+                      <PiMoonFill className="mr-2 size-4" />
+                    ) : (
+                      <PiMoon className="mr-2 size-4" />
+                    )}
+                    Dark
+                  </DropdownMenuItem>
+                </DropdownMenuSubContent>
+              </DropdownMenuPortal>
+            </DropdownMenuSub>
+            <DropdownMenuSeparator />
+            <AlertDialogTrigger className="flex w-full flex-row">
+              <DropdownMenuItem className="w-full cursor-pointer">
+                <PiSignOut className="mr-2 size-4" />
+                Sign Out
+              </DropdownMenuItem>
+            </AlertDialogTrigger>
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Ready to log out?</AlertDialogTitle>
+            <AlertDialogDescription>
+              You’re just a click away from signing out. Don’t worry, we’ll be
+              here when you’re ready to come back. Want to continue?
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction onClick={() => signOut()}>
+              Continue
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
     );
   }
 

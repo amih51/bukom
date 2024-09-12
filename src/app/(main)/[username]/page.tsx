7970538390ab3094c7829b41ef9e1f64dce79c2d
Tuyulll <19623185@std.stei.itb.ac.id>
2024-09-12 08:23:48 +0700

    feat: profile background

--- src/app/(main)/[username]/page.tsx
+++ src/app/(main)/[username]/page.tsx
@@ -2,6 +2,9 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Metadata } from "next";
 import ProfileFeed from "./profile-feed";
 import { prisma } from "@/lib/prisma";
+import { Button } from "@/components/ui/button";
+import { getSession } from "next-auth/react";
+import { auth } from "@/auth";
 
 const getUser = async (username: string) => {
   const user = await prisma.user.findFirst({
@@ -32,24 +35,28 @@ export default async function Page({
   params: { username: string };
 }) {
   const user = await getUser(username);
+  const session = await auth();
 
   return (
     <main className="flex w-full flex-col">
-      <div>
-        <div>
-          <Avatar>
-            <AvatarImage src={user?.image || ""} />
-            <AvatarFallback>{user?.username}</AvatarFallback>
-          </Avatar>
-        </div>
-        <div>
-          <div className="w-full">
-            <div className="ml-4 hidden flex-col overflow-hidden text-left lg:flex">
+      <div className="my-2 flex w-full flex-row overflow-hidden border-2 sm:border-l-0">
+        <Avatar className="m-2 size-32">
+          <AvatarImage src={session?.user?.image || ""} />
+          <AvatarFallback>{session?.user.username}</AvatarFallback>
+        </Avatar>
+        <div className="flex w-full flex-col overflow-hidden border-l-2">
+          <div className="flex w-full flex-row border-b-2">
+            <div className="mx-2 flex w-full flex-row items-center">
               <p className="truncate text-lg">{user?.name}</p>
-              <p className="truncate text-sm opacity-50">@{user?.username}</p>
+              <p className="truncate pl-2 text-sm opacity-50">
+                @{user?.username}
+              </p>
             </div>
+            <Button variant={"ghost"} className="border-l-2">
+              edit
+            </Button>
           </div>
-          {username === user?.username && <div>edit</div>}
+          <div>bio</div>
         </div>
       </div>
       {user?.id && <ProfileFeed userId={user.id} />}

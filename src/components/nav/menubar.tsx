3021610ea078654e40dffe4cde6b63ae9fab2e22
Tuyulll <19623185@std.stei.itb.ac.id>
2024-09-13 14:17:31 +0700

    fix: link to router

--- src/components/nav/menubar.tsx
+++ src/components/nav/menubar.tsx
@@ -14,8 +14,7 @@ import {
   RiUserFill,
   RiUserLine,
 } from "react-icons/ri";
-import { usePathname } from "next/navigation";
-import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
 import { CreatePostDialog } from "../post/editor/create-post-dialog";
 import { useTheme } from "next-themes";
 import Image from "next/image";
@@ -24,6 +23,7 @@ const MenuBar = forwardRef<HTMLDivElement, { className: string }>(
   ({ className }, ref) => {
     const { theme } = useTheme();
     const { data: session } = useSession();
+    const router = useRouter();
     const user = session?.user;
 
     const pathName = usePathname();
@@ -32,7 +32,11 @@ const MenuBar = forwardRef<HTMLDivElement, { className: string }>(
       <div ref={ref} className={className}>
         <div className="flex w-full flex-col justify-between sm:w-fit lg:w-full">
           <div className="flex w-full flex-row justify-between px-6 sm:flex-col sm:gap-3 sm:px-0">
-            <Link href="/" className="my-6 hidden text-2xl font-bold lg:block">
+            <Button
+              onClick={() => router.push("/")}
+              variant={"ghost"}
+              className="my-6 hidden h-fit items-start text-2xl font-bold hover:bg-transparent lg:block"
+            >
               {theme === "dark" ? (
                 <Image
                   src={"/HMIFess-white-02.png"}
@@ -55,70 +59,60 @@ const MenuBar = forwardRef<HTMLDivElement, { className: string }>(
                   height={192}
                 />
               )}
-            </Link>
+            </Button>
             <Button
+              onClick={() => router.push("/home")}
               variant={pathName === "/home" ? "secondary" : "ghost"}
-              className="flex h-fit w-fit items-center lg:w-full"
+              className="flex h-fit w-fit items-center justify-start text-lg lg:w-full"
             >
-              <Link href="/home" className="flex size-full flex-row text-lg">
-                {pathName === "/home" ? (
-                  <RiHomeFill className="size-6 flex-shrink-0" />
-                ) : (
-                  <RiHomeLine className="size-6 flex-shrink-0" />
-                )}
+              {pathName === "/home" ? (
+                <RiHomeFill className="size-6 flex-shrink-0" />
+              ) : (
+                <RiHomeLine className="size-6 flex-shrink-0" />
+              )}
 
-                <p className="ml-2 hidden truncate lg:inline">Home</p>
-              </Link>
+              <p className="ml-2 hidden truncate lg:inline">Home</p>
             </Button>
             <Button
+              onClick={() => router.push("/search")}
               variant={pathName === "/search" ? "secondary" : "ghost"}
-              className="flex h-fit w-fit items-center lg:w-full"
+              className="flex h-fit w-fit items-center justify-start text-lg lg:w-full"
             >
-              <Link href="/search" className="flex size-full flex-row text-lg">
-                {pathName === "/search" ? (
-                  <RiSearchFill className="size-6 flex-shrink-0" />
-                ) : (
-                  <RiSearchLine className="size-6 flex-shrink-0" />
-                )}
+              {pathName === "/search" ? (
+                <RiSearchFill className="size-6 flex-shrink-0" />
+              ) : (
+                <RiSearchLine className="size-6 flex-shrink-0" />
+              )}
 
-                <p className="ml-2 hidden truncate lg:inline">Search</p>
-              </Link>
+              <p className="ml-2 hidden truncate lg:inline">Search</p>
             </Button>
             <Button
+              onClick={() => router.push("/bookmarks")}
               variant={pathName === "/bookmarks" ? "secondary" : "ghost"}
-              className="flex h-fit w-fit items-center lg:w-full"
+              className="flex h-fit w-fit items-center justify-start text-lg lg:w-full"
             >
-              <Link
-                href="/bookmarks"
-                className="flex size-full flex-row text-lg"
-              >
-                {pathName === "/bookmarks" ? (
-                  <RiBookmarkFill className="size-6 flex-shrink-0" />
-                ) : (
-                  <RiBookmarkLine className="size-6 flex-shrink-0" />
-                )}
+              {pathName === "/bookmarks" ? (
+                <RiBookmarkFill className="size-6 flex-shrink-0" />
+              ) : (
+                <RiBookmarkLine className="size-6 flex-shrink-0" />
+              )}
 
-                <p className="ml-2 hidden truncate lg:inline">Bookmarks</p>
-              </Link>
+              <p className="ml-2 hidden truncate lg:inline">Bookmarks</p>
             </Button>
             <Button
+              onClick={() => router.push(`/${user?.username}`)}
               variant={
                 pathName === `/${user?.username}` ? "secondary" : "ghost"
               }
-              className="flex h-fit w-fit items-center lg:w-full"
+              className="flex h-fit w-fit items-center justify-start text-lg lg:w-full"
             >
-              <Link
-                href={`/${user?.username}`}
-                className="flex size-full flex-row text-lg"
-              >
-                {pathName === `/${user?.username}` ? (
-                  <RiUserFill className="size-6 flex-shrink-0" />
-                ) : (
-                  <RiUserLine className="size-6 flex-shrink-0" />
-                )}
+              {pathName === `/${user?.username}` ? (
+                <RiUserFill className="size-6 flex-shrink-0" />
+              ) : (
+                <RiUserLine className="size-6 flex-shrink-0" />
+              )}
 
-                <p className="ml-2 hidden truncate lg:inline">Profile</p>
-              </Link>
+              <p className="ml-2 hidden truncate lg:inline">Profile</p>
             </Button>
             <CreatePostDialog />
           </div>

6ee502e5f1917e201e5a86a858544d0cea9c459e
Tuyulll <19623185@std.stei.itb.ac.id>
2024-09-14 17:53:18 +0700

    fix: vercel

--- src/app/(main)/home/page.tsx
+++ src/app/(main)/home/page.tsx
@@ -1,5 +1,7 @@
 import { Metadata } from "next";
 import Feed from "./feed";
+import { Suspense } from "react";
+import { LuLoader } from "react-icons/lu";
 
 export const metadata: Metadata = {
   title: {
@@ -8,10 +10,12 @@ export const metadata: Metadata = {
   },
 };
 
-export default async function Page() {
+export default function Page() {
   return (
     <main className="flex w-full flex-col">
-      <Feed />
+      <Suspense fallback={<LuLoader className="mx-auto my-3 animate-spin" />}>
+        <Feed />
+      </Suspense>
     </main>
   );
 }

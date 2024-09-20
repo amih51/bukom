2d885bcbbc2285b10328404d5566801faf091869
Tuyulll <19623185@std.stei.itb.ac.id>
2024-09-20 12:00:58 +0700

    style: search field

--- src/components/nav/navbar.tsx
+++ src/components/nav/navbar.tsx
@@ -4,8 +4,11 @@ import Link from "next/link";
 import ProfileButton from "./profile-btn";
 import SearchField from "../search-field";
 import Logo from "../logo";
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
+  const pathname = usePathname();
+
   return (
     <header className="sticky top-0 z-30 flex h-14 flex-row items-center justify-between border-b-2 bg-background px-6 lg:hidden">
       <div className="flex h-full w-28 flex-wrap items-center lg:w-[calc(20%+4rem)]">
@@ -16,9 +19,11 @@ export default function Navbar() {
         </Link>
       </div>
       <div className="hidden size-full items-center sm:flex">
-        <div className="w-full">
-          <SearchField />
-        </div>
+        {pathname != "/search" && (
+          <div className="w-full">
+            <SearchField />
+          </div>
+        )}
       </div>
       <div className="flex h-full items-center sm:hidden">
         <ProfileButton />
--- src/components/search-field.tsx
+++ src/components/search-field.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { usePathname, useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { Input } from "./ui/input";
 import { RiSearchFill, RiSearchLine } from "react-icons/ri";
 import { Button } from "./ui/button";
@@ -8,7 +8,6 @@ import Link from "next/link";
 
 export default function SearchField() {
   const router = useRouter();
-  const pathName = usePathname();
 
   function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
@@ -21,15 +20,9 @@ export default function SearchField() {
   return (
     <form onSubmit={handleSubmit} method="GET" action="/search">
       <div className="flex flex-row items-center">
-        <Button variant={"ghost"} className="flex size-fit">
-          <Link href="/search" className="flex size-fit">
-            {pathName === "/search" ? (
-              <RiSearchFill className="size-6 flex-shrink-0" />
-            ) : (
-              <RiSearchLine className="size-6 flex-shrink-0" />
-            )}
-          </Link>
-        </Button>
+        <Link href="/search" className="ml-3 flex size-fit">
+          <RiSearchLine className="size-6 flex-shrink-0" />
+        </Link>
         <Input name="q" placeholder="Search" className="pl-2" />
       </div>
     </form>

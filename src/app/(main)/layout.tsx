30d42b53eef73cbe25ee238ed3a5cda48403da19
Tuyulll <19623185@std.stei.itb.ac.id>
2024-09-20 11:53:46 +0700

    fix: nav height

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -10,7 +10,7 @@ export default async function Layout({
     <div className="flex min-h-screen flex-shrink-0 flex-col bg-background">
       <Navbar />
       <div className="flex flex-grow flex-row">
-        <MenuBar className="sticky left-0 top-12 hidden max-h-[calc(100vh-48px)] w-fit flex-grow pt-3 sm:flex sm:border-r-2 md:px-6 lg:top-0 lg:max-h-[calc(100vh)] lg:w-1/5" />
+        <MenuBar className="sticky left-0 top-12 hidden max-h-[calc(100vh-56px)] w-fit flex-grow pt-3 sm:flex sm:border-r-2 md:px-6 lg:top-0 lg:max-h-[calc(100vh)] lg:w-1/5" />
         <div className="w-full px-6">{children}</div>
       </div>
       <MenuBar className="sticky bottom-0 flex w-full border-t-2 bg-background py-2 sm:hidden" />

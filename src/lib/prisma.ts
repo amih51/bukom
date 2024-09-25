aba523ab92102b2b5ce08803ca0d84d5a33cc449
Tuyulll <19623185@std.stei.itb.ac.id>
2024-09-25 22:46:05 +0700

    feat: prisma accelerate

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,12 @@
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client/edge";
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+// const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+// export const prisma = globalForPrisma.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+
+export const prisma = new PrismaClient().$extends(withAccelerate());

9d6e213a3da3e665a9856993def1af4eb7a8ea49
Tuyulll <19623185@std.stei.itb.ac.id>
2024-09-27 11:16:06 +0700

    style: create post dialog

--- src/components/post/editor/create-post-dialog.tsx
+++ src/components/post/editor/create-post-dialog.tsx
@@ -124,16 +124,8 @@ export function CreatePostDialog() {
           <div className="mx-6">
             <DialogTitle className="mb-5 text-xl">Create Menfess</DialogTitle>
             <div className="flex w-full flex-row">
-              {isAnon ? (
-                <BsIncognito className="mr-6 hidden size-12 sm:block" />
-              ) : (
-                <Avatar className="mr-5 hidden size-12 sm:block">
-                  <AvatarImage src={user?.image || ""} />
-                  <AvatarFallback>{user?.username}</AvatarFallback>
-                </Avatar>
-              )}
               <div className="flex w-full flex-col gap-5 overflow-auto">
-                <p className="sm:hidden">
+                <div>
                   {isAnon ? (
                     <div className="flex flex-row items-center">
                       <BsIncognito className="mr-3 size-8" />
@@ -155,7 +147,7 @@ export function CreatePostDialog() {
                       </p>
                     </div>
                   )}
-                </p>
+                </div>
                 <EditorContent
                   editor={editor}
                   className="max-h-[75vh] min-h-52 overflow-auto rounded-lg border border-border p-2 scrollbar scrollbar-thumb-current scrollbar-w-1 hover:scrollbar-thumb-foreground/50"

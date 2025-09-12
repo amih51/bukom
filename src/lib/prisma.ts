8d985165725842544f466fcc40ce00454454be47
jarv1s <19623185@std.stei.itb.ac.id>
2025-09-12 18:58:46 +0700

    fix: update global prisma variable declaration and initialization logic

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient};
+declare global {
+	var prisma: PrismaClient | undefined;
+}
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+const prisma = global.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+
+export { prisma };
